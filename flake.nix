542d39918f6351541f0cf423fb89b4157a944dc9
Lee Olayvar <leeola@users.noreply.github.com>
2025-01-15 11:01:38 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [ "rust-analysis" ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            pkg-config
+            binutils
+            gcc
+            rust-analyzer
+            # using a hardcoded rustfmt version to support nightly rustfmt features.
+            rust-bin.nightly."2025-01-09".rustfmt
+            rust-toolchain
+          ];
+        };
+      }
+    );
+}
